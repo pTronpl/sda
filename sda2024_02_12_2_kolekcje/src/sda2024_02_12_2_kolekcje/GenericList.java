9fb6095d279597413b62e091f1edab62ae62ac70
pTronpl <159312672+pTronpl@users.noreply.github.com>
2024-02-12 19:59:57 +0100

    Add files via upload

--- sda2024_02_12_2_kolekcje/src/sda2024_02_12_2_kolekcje/GenericList.java
+++ sda2024_02_12_2_kolekcje/src/sda2024_02_12_2_kolekcje/GenericList.java
@@ -7,18 +7,11 @@ import java.util.List;
 import java.util.Set;
 
 public class GenericList<T> {
-	private T element;
-	private List<T> list;
-	private Set<T> set;
 	
 	GenericList(){
-		this.set = new HashSet<T>();
-		this.list = new ArrayList<T>();
 	}
 	
 	public List<T> uniqeList(Collection<? extends T> list){
-		 this.set.addAll(list);
-		 this.list.addAll(set);
-		 return this.list;
+		return new ArrayList<T> (new HashSet<>(list));
 	}
 }
